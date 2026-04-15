a43c43f70cace0f4fc0e186dd7af106ab2652df5
muhammedarsathcn <muhammed.arsathcn@cdw.com>
2026-04-15 16:55:01 +0530

    folder structure created

---
+++ npm-practice/index.js
@@ -0,0 +1,36 @@
+import fs from "fs";
+import http from "http";
+import chalk from "chalk";
+
+const server = http.createServer((req, res) => {
+  if (req.url === "/colors" && req.method === "GET") {
+    fs.readFile("./data/color_ palette.json", "utf-8", (err, data) => {
+      if (err) {
+        res.writeHead(500, { "content-type": "application/json" });
+        res.end(JSON.stringify({ message: "Error in reading the file" }));
+      }
+      const colors = JSON.parse(data);
+      const shuffledColors = colors.sort(() => 0.5 - Math.random());
+      const fiveShuffledColors = shuffledColors.slice(0, 5);
+      res.writeHead(200, { "content-type": "application/json" });
+      res.end(
+        JSON.stringify({
+          message: "Random five colors",
+          data: fiveShuffledColors,
+        }),
+      );
+    });
+  } else {
+    res.writeHead(404, { "Content-Type": "application/json" });
+    res.end(
+      JSON.stringify({
+        message: "Route not found. Use /colors",
+      }),
+    );
+  }
+});
+
+
+server.listen(3000, () => {
+    console.log("Server is running on http://localhost:3000")
+})
