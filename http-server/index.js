e930e9177b97a4acfaf698d5d31af9fa59f777fe
muhammedarsathcn <muhammed.arsathcn@cdw.com>
2026-04-16 14:48:52 +0530

    shuffledColors logic reduced to single line

--- http-server/index.js
+++ http-server/index.js
@@ -1,6 +1,5 @@
 const fs = require("fs");
 const http = require("http");
-
 //server creation
 const server = http.createServer((req, res) => {
   if (req.url === "/colors" && req.method === "GET") {
@@ -10,13 +9,12 @@ const server = http.createServer((req, res) => {
         res.end(JSON.stringify({ message: "Error in reading the file" }));
       }
       const colors = JSON.parse(data);
-      const shuffledColors = colors.sort(() => 0.5 - Math.random());
-      const fiveShuffledColors = shuffledColors.slice(0, 5);
+      const shuffledColors = colors.sort(() => 0.5 - Math.random()).slice(0,5)
       res.writeHead(200, { "content-type": "application/json" });
       res.end(
         JSON.stringify({
           message: "Random five colors",
-          data: fiveShuffledColors,
+          data: shuffledColors,
         }),
       );
     });
